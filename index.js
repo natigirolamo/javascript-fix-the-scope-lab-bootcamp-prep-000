d2e1164b3a749c6ec1ee1ed3bab2057902a4cea9
exuberant-glitch-6816 <natalia.girolamo@gmail.com>
2019-05-05 21:32:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,14 +17,21 @@ function add2(n) {
 
   // Feel free to move things around!
   const two = 2
+   return parseInt(n, 10) + two
+ 
+  // Feel free to move things around!
+ 
 }
-
+ 
 var funkyFunction = function() {
   return function() {
     return "FUNKY!"
   }
 }
+ 
+// We want 'funkyFunction' on the line below to return a function that returns "FUNKY!" -- how can we accomplish that?
+// NOTE: To pass this final test, you only need to modify the code below this line.
+var theFunk = funkyFunction()
+theFunk = theFunk()
+}
 
-// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
